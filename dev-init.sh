58d84c3d1af4970dd9fce10ce6ffdb1d07baf41a
Alex Skrypnyk <alex.designworks@gmail.com>
2018-12-28 12:58:30 +1100

    Updated dev init.

--- dev-init.sh
+++ dev-init.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 ##
-# Setup project for development.
+# Install development files from the centralised location.
+#
+# Files already committed within current repository will not be overridden.
+##
+# To override any files coming from Dev Tools to persist in the current
+# repository, modify `.git/info/exclude` file and commit them.
 #
 # Usage:
 # . dev-init.sh
 #
 
-# Development only: uncomment and set commit value to fetch Dev Tools at
+# Development only: uncomment and set the commit value to fetch Dev Tools at
 # specific commit.
 #export GH_COMMIT=COMMIT_SHA
 
-# Install development files, including the scripts used below, from the
-# centralised location.
 curl https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install | bash
