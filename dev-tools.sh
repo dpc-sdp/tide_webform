2d99083ecca33538a389472f27a80b2d7b683f78
Edward Yuen <edward.yuen@dpc.vic.gov.au>
2023-06-08 16:27:29 +1000

    [SDPAP-7134] Webform default submit label.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=70b73d6c169cbc457351745634105402ab326a27
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
