cd097f29806fa64a2613dd956e26120650ad2d56
Edward Yuen <edward.yuen@dpc.vic.gov.au>
2023-06-09 11:25:04 +1000

    Reverts dev-tool SHA.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-export GH_COMMIT=70b73d6c169cbc457351745634105402ab326a27
+# export GH_COMMIT=COMMIT_SHA
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
