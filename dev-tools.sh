7346ad44358ae26eabd5261f5f83c3a30ced1e2a
Edward Yuen <edward.yuen@dpc.vic.gov.au>
2023-06-16 15:28:33 +1000

    Reverst - To fix Use statement order fix.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-export GH_COMMIT=64910ee669914fa3976009675ad8da45c94eefd9
+# export GH_COMMIT=COMMIT_SHA
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
